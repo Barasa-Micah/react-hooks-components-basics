089d987076f5a0b43f222c3a39bb64b42a94db6e
Barasa-Micah <barasamica@gmail.com>
2023-07-19 23:53:50 +0300

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
+// import React from "react";
+// import Article from "./Article";
+// import Comment from "./Comment";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <Article />
+//       <Comment />
+//     </div>
+//   );
+// }
+
+// export default App;
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 200,
+};
+
+export default function Profile() {
   return (
-    <div>
-      <Article />
-      <Comment />
-    </div>
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
 }
-
-export default App;
